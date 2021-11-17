fce070922a3a033735fbcd8f386f2665695d2475
Roman Kolosov <lonelydriver_37@inbox.ru>
2021-11-17 16:29:52 +0300

    updated 11/17/2021

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,17 @@
-git add .
-
 echo 'Загружаю лекции...'
+
+cd C:\Users\lonel\Documents\Академия\3-course
+
+git init
+
+git pull
+
+git add --all
 
 git commit -m "updated `date +'%m/%d/%Y'`"
 
-git push -q origin master
+git push -q
 
 echo 'Лекции обновлены'
 
+exit
